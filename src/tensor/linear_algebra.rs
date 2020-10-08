aa7ea0bcd72814db7e791703a9eb49f0ba3b0070
Lucas Bourtoule <lucas@bourtoule.com>
2020-10-08 13:03:11 +0200

    fix matrix matrix dynamic dot impl and add matrix vector and vector vector dot products

--- src/tensor/linear_algebra.rs
+++ src/tensor/linear_algebra.rs
@@ -3,10 +3,10 @@ extern crate openblas_src;
 
 use super::allocation_policy::{DynamicAllocationPolicy, StaticAllocationPolicy};
 use super::layout::Layout;
-use super::shape::{Shape2D, TRUE};
+use super::shape::{Shape1D, Shape2D, TRUE};
 use super::tensor::Tensor;
 use super::transpose_policy::{Contiguous, BLASPolicy};
-use cblas::{dgemm, sgemm};
+use cblas::{dgemm, sgemm, dgemv, sgemv, sdot, ddot};
 use road_ai_macros::expand_operations;
 use typenum::{Eq, IsEqual, Unsigned};
 
@@ -87,12 +87,12 @@ where
                 self_shape[1] as i32,
                 1.0,
                 self,
-                M::I32,
+                self_shape[1] as i32,
                 other,
                 N::I32,
                 1.0,
                 &mut out,
-                M::I32,
+                N::I32,
             );
         }
 
@@ -131,15 +131,195 @@ where
                 self_shape[1] as i32,
                 1.0,
                 self,
-                self_shape[0] as i32,
+                self_shape[1] as i32,
                 other,
                 other_shape[1] as i32,
                 1.0,
                 &mut out,
-                self_shape[0] as i32,
+                other_shape[1] as i32,
             );
         }
 
         out
+    }
+}
+
+#[expand_operations(
+    dgemv<T=f64> as dotv,
+    sgemv<T=f32> as dotv,
+)]
+impl<T, M, N, C, L, P> Tensor<T, Shape2D<M, N>, C, L, P>
+where
+    L: for<'a> Layout<'a, T>,
+    C: BLASPolicy,
+{
+    pub fn operation<Crhs, Lrhs, Prhs>(
+        &self,
+        other: &Tensor<T, Shape1D<N>, Crhs, Lrhs, Prhs>,
+    ) -> Tensor<T, Shape1D<M>, Contiguous, P::Layout, P>
+    where
+        M: Unsigned,
+        N: Unsigned,
+        Lrhs: for<'a> Layout<'a, T>,
+        P: StaticAllocationPolicy<T, Shape1D<M>>,
+    {
+        let mut out: Tensor<T, Shape1D<M>, Contiguous, P::Layout, P> = Tensor::default();
+
+        unsafe {
+            placeholder(
+                cblas::Layout::RowMajor,
+                C::BLAS_TRANSPOSE,
+                M::I32,
+                N::I32,
+                1.0,
+                self,
+                N::I32,
+                other,
+                1,
+                1.0,
+                &mut out,
+                1,
+            );
+        }
+
+        out
+    }
+
+    pub fn coerce<Nrhs, Crhs, Lrhs, Prhs>(
+        &self,
+        other: &Tensor<T, Shape1D<Nrhs>, Crhs, Lrhs, Prhs>,
+    ) -> Tensor<T, Shape1D<M>, Contiguous, P::Layout, P>
+    where
+        M: Unsigned,
+        N: IsEqual<Nrhs>,
+        Eq<N, Nrhs>: TRUE,
+        Lrhs: for<'a> Layout<'a, T>,
+        P: StaticAllocationPolicy<T, Shape1D<M>>,
+    {
+        let self_shape = self.shape();
+        let other_shape = other.shape();
+        assert_eq!(
+            self_shape[1], other_shape[0],
+            "Contracted dimmensions {} and {} must be equal, got shapes {:?} and {:?}.",
+            self_shape[1], other_shape[0], self_shape, other_shape,
+        );
+        let mut out: Tensor<T, Shape1D<M>, Contiguous, P::Layout, P> = Tensor::default();
+
+        unsafe {
+            placeholder(
+                cblas::Layout::RowMajor,
+                C::BLAS_TRANSPOSE,
+                M::I32,
+                self_shape[1] as i32,
+                1.0,
+                self,
+                self_shape[1] as i32,
+                other,
+                1,
+                1.0,
+                &mut out,
+                1,
+            );
+        }
+
+        out
+    }
+
+    pub fn dynamic<Nrhs, Crhs, Lrhs, Prhs>(
+        &self,
+        other: &Tensor<T, Shape1D<Nrhs>, Crhs, Lrhs, Prhs>,
+    ) -> Tensor<T, Shape1D<M>, Contiguous, P::Layout, P>
+    where
+        N: IsEqual<Nrhs>,
+        Eq<N, Nrhs>: TRUE,
+        Lrhs: for<'a> Layout<'a, T>,
+        P: DynamicAllocationPolicy<T>,
+    {
+        let self_shape = self.shape();
+        let other_shape = other.shape();
+        assert_eq!(
+            self_shape[1], other_shape[0],
+            "Contracted dimmensions {} and {} must be equal, got shapes {:?} and {:?}.",
+            self_shape[1], other_shape[0], self_shape, other_shape,
+        );
+
+        let mut out: Tensor<T, Shape1D<M>, Contiguous, P::Layout, P> =
+            Tensor::alloc(vec![self_shape[0]]);
+
+        unsafe {
+            placeholder(
+                cblas::Layout::RowMajor,
+                C::BLAS_TRANSPOSE,
+                self_shape[0] as i32,
+                self_shape[1] as i32,
+                1.0,
+                self,
+                self_shape[1] as i32,
+                other,
+                1,
+                1.0,
+                &mut out,
+                1,
+            );
+        }
+
+        out
+    }
+}
+
+#[expand_operations(
+    ddot<T=f64> as dot,
+    sdot<T=f32> as dot,
+)]
+impl<T, N, C, L, P> Tensor<T, Shape1D<N>, C, L, P>
+where
+    L: for<'a> Layout<'a, T>,
+    C: BLASPolicy,
+{
+    pub fn operation<Crhs, Lrhs, Prhs>(
+        &self,
+        other: &Tensor<T, Shape1D<N>, Crhs, Lrhs, Prhs>,
+    ) -> T
+    where
+        N: Unsigned,
+        Lrhs: for<'a> Layout<'a, T>,
+    {
+        unsafe {
+            placeholder(
+                N::I32,
+                self,
+                1,
+                other,
+                1,
+            )
+        }
+    }
+
+    pub fn dynamic<Nrhs, Crhs, Lrhs, Prhs>(
+        &self,
+        other: &Tensor<T, Shape1D<Nrhs>, Crhs, Lrhs, Prhs>,
+    ) -> T
+    where
+        N: IsEqual<Nrhs>,
+        Eq<N, Nrhs>: TRUE,
+        Lrhs: for<'a> Layout<'a, T>,
+    {
+        let self_shape = self.shape();
+        let other_shape = other.shape();
+        assert_eq!(
+            self_shape[0], other_shape[0],
+            "Contracted dimmensions {} and {} must be equal, got shapes {:?} and {:?}.",
+            self_shape[0], other_shape[0], self_shape, other_shape,
+        );
+
+        unsafe {
+            placeholder(
+                self_shape[0] as i32,
+                self,
+                1,
+                other,
+                1,
+            )
+        }
     }
 }
